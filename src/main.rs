a8fdce221907b58f2695126b0ae3ef550006f931
Callum <Callum@cantonim.me>
2023-03-24 11:32:39 +0000

    Implementing Control Sequences

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,15 @@ enum SelectGraphicRendition {
     NethirSuperOrSubScript,
 }
 
-enum ControlSequences {
+impl SelectGraphicRendition {
+
+    pub fn from(args: Vec<u16>) -> Option<SelectGraphicRendition> {
+        todo!("Add SGR Parsing");
+        return None;
+    }
+}
+
+enum ControlSequence {
     CursorUp(u16),
     CursorDown(u16),
     CursorForward(u16),
@@ -78,47 +86,185 @@ enum ControlSequences {
     AUXPortOn,
     AUXPortOff,
     DeviceStatusReport,
+    SaveCursorPosistion,
+    RestoreCursorPosistion,
+    VT220Cursor,
+    HideCursor,
+    EnableReportingFocus,
+    DisableReportingFocus,
+    EnableAltScreenBuf,
+    DisableAltScreenBuf,
+    BracketPasteMode,
+    NoBracketPasteMode,
 }
 
-impl ControlSequences {
-    pub fn from(chars: &mut Chars) -> ControlSequences {
-        return ControlSequences::DeviceStatusReport;
+impl ControlSequence {
+    pub fn get_args(text: &String) -> Vec<u16> {
+        todo!("Implemnt args parsing");
+        return Vec::new();
+    }
+    pub fn from(chars: &mut Chars) -> Option<ControlSequence> {
+        let mut text_buffer = String::new();
+        while let Some(c) = chars.next() {
+            match c {
+                'm' => {}
+                'A' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::CursorUp(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'B' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::CursorDown(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'C' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::CursorForward(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'D' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::CursorBack(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'E' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::CursorNextLine(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'F' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::CursorPreviousLine(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'G' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::CursorHorizontalAbsolute(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'H' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::CursorPosition(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                        args.get(1).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'J' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::EraseInDisplay(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'K' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::EraseInLine(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'S' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::ScrollUp(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'T' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::ScrollDown(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'f' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return Some(ControlSequence::HorizonalVerticalPosition(
+                        args.get(0).unwrap_or(&(1 as u16)).clone(),
+                        args.get(1).unwrap_or(&(1 as u16)).clone(),
+                    ));
+                }
+                'i' => {
+                    let args = ControlSequence::get_args(&text_buffer);
+                    return match args.get(0) {
+                        Some(arg) => match arg {
+                            5 => Some(ControlSequence::AUXPortOn),
+                            4 => Some(ControlSequence::AUXPortOff),
+                            _ => None,
+                        },
+                        None => None,
+                    };
+                }
+                'h' => {
+                    return match text_buffer.as_str() {
+                        "?25" => Some(ControlSequence::VT220Cursor),
+                        "?1004" => Some(ControlSequence::EnableReportingFocus),
+                        "?1049" => Some(ControlSequence::EnableAltScreenBuf),
+                        "?2004" => Some(ControlSequence::BracketPasteMode),
+                        _ => None,
+                    }
+                }
+                'l' => {
+                    return match text_buffer.as_str() {
+                        "?25" => Some(ControlSequence::HideCursor),
+                        "?1004" => Some(ControlSequence::DisableReportingFocus),
+                        "?1049" => Some(ControlSequence::DisableAltScreenBuf),
+                        "?2004" => Some(ControlSequence::NoBracketPasteMode),
+                        _ => None,
+                    }
+                }
+                'n' => return Some(ControlSequence::DeviceStatusReport),
+                's' => return Some(ControlSequence::SaveCursorPosistion),
+                'u' => return Some(ControlSequence::RestoreCursorPosistion),
+                _ => {
+                    text_buffer.push(c);
+                }
+            }
+        }
+        return None;
     }
 }
 
-enum FeEscapeSequences {
+enum FeEscapeSequence {
     SingleShiftTwo,
     SingleShiftThree,
     DeviceControlString,
-    ControlSequence(ControlSequences),
+    ControlSequence(ControlSequence),
     StartOfString,
     PrivacyMessage,
     ApplicationProgramCommand,
 }
 
-impl FeEscapeSequences {
-    pub fn from(chars: &mut Chars) -> Option<FeEscapeSequences> {
+impl FeEscapeSequence {
+    pub fn from(chars: &mut Chars) -> Option<FeEscapeSequence> {
         return match chars.next() {
             Some(c) => match c {
-                'N' | 'n' => Some(FeEscapeSequences::SingleShiftTwo),
-                'O' | 'o' => Some(FeEscapeSequences::SingleShiftThree),
-                'P' | 'p' => Some(FeEscapeSequences::DeviceControlString),
-                '[' => Some(FeEscapeSequences::ControlSequence(ControlSequences::from(
-                    chars,
-                ))),
-                'X' | 'x' => Some(FeEscapeSequences::StartOfString),
-                '^' => Some(FeEscapeSequences::PrivacyMessage),
-                '_' => Some(FeEscapeSequences::ApplicationProgramCommand),
+                'N' | 'n' => Some(FeEscapeSequence::SingleShiftTwo),
+                'O' | 'o' => Some(FeEscapeSequence::SingleShiftThree),
+                'P' | 'p' => Some(FeEscapeSequence::DeviceControlString),
+                '[' => match ControlSequence::from(chars) {
+                    Some(controlsequence) => {
+                        Some(FeEscapeSequence::ControlSequence(controlsequence))
+                    }
+                    None => None,
+                },
+                'X' | 'x' => Some(FeEscapeSequence::StartOfString),
+                '^' => Some(FeEscapeSequence::PrivacyMessage),
+                '_' => Some(FeEscapeSequence::ApplicationProgramCommand),
                 _ => None,
             },
             None => None,
         };
     }
 
-    pub fn extract_from(string: &str) -> (String, Option<FeEscapeSequences>) {
+    pub fn extract_from(string: &str) -> (String, Option<FeEscapeSequence>) {
         let mut chars = string.chars();
         if chars.next().unwrap() == ESC {
-            let esc_seq = FeEscapeSequences::from(&mut chars);
+            let esc_seq = FeEscapeSequence::from(&mut chars);
             return (chars.as_str().to_string(), esc_seq);
         }
         return (string.to_string(), None);
@@ -127,7 +273,7 @@ impl FeEscapeSequences {
 
 enum ANSITextElement {
     Text(String),
-    EscapeSequence(FeEscapeSequences),
+    EscapeSequence(FeEscapeSequence),
 }
 
 struct ANSIText {
@@ -142,7 +288,7 @@ impl ANSIText {
             if sequence.len() <= 0 {
                 continue;
             }
-            let (text, opt_fe_sequence) = FeEscapeSequences::extract_from(sequence);
+            let (text, opt_fe_sequence) = FeEscapeSequence::extract_from(sequence);
             if let Some(fe_sequence) = opt_fe_sequence {
                 text_buffer
                     .text
