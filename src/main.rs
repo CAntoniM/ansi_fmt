8f736a8e2e5ac6cf7d823fa88029e630d76e3b57
Callum <Callum@cantonim.me>
2023-03-28 22:25:41 +0100

    adding an implementation of the main method.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use std::{
     fs::File,
-    io::{self, BufRead},
+    io::{self, BufRead, Write},
     path::PathBuf,
 };
 
@@ -27,16 +27,58 @@ struct App {
 }
 
 impl App {
+    pub fn write(&self, string: &String) -> Result<(), &'static str> {
+        match self.output.clone() {
+            Some(path) => {
+                match std::fs::OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .create(true)
+                    .open(path)
+                {
+                    Ok(mut file) => {
+                        if let Err(e) = file.write(string.as_bytes()) {
+                            return Err("Failed to write to file");
+                        }
+                        return Ok(());
+                    }
+                    Err(e) => {
+                        return Err("failed to open file");
+                    }
+                }
+            }
+            None => {
+                if let Err(e) = std::io::stdout().write(string.as_bytes()) {
+                    return Err("failed to write to standard out");
+                }
+                return Ok(());
+            }
+        }
+    }
+
+    pub fn parse_text(&self, string: String) -> Result<(), &'static str> {
+        let mut ansi_text = input_fmt::ansi::Text::new();
+        ansi_text.read(string);
+        match output_fmt::from(self.format, internal_format::Text::from_ansi(ansi_text)) {
+            Some(formater) => {
+                if let Err(e) = self.write(&formater.to_string()) {
+                    return Err(e);
+                }
+                return Ok(());
+            }
+            None => return Err("Failed to find a writer for the given output format."),
+        }
+    }
+
     pub fn run(&self) -> Result<(), &'static str> {
         for path in self.paths.iter() {
             let file = File::open(path).unwrap();
             let reader = io::BufReader::new(file);
-            let mut ansi_text = input_fmt::ansi::Text::new();
-            for line in reader.lines() {
-                ansi_text.read(line.unwrap());
+            if let Err(e) = self.parse_text(std::io::read_to_string(reader).unwrap()) {
+                return Err(e);
             }
         }
-        return Ok(());
+        return self.parse_text(std::io::read_to_string(std::io::stdin()).unwrap());
     }
 }
 
